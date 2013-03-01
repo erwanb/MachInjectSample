6b89e2adf6ab716b3e3805cdcf9e79baaf9cf590
RudyAramayo <rudy@klinkcdc.com>
2013-03-01 17:13:03 -0600

    more cleanup

--- MachInjectSample/DKAppDelegate.m
+++ MachInjectSample/DKAppDelegate.m
@@ -16,37 +16,39 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-  NSError *error;
-  
-  // Install helper tools
-  if ([DKInstaller isInstalled] == NO &&
-      [DKInstaller install:&error] == NO)
-  {
-    assert(error != nil);
+    NSError *error;
     
-    NSLog(@"Couldn't install MachInjectSample (domain: %@ code: %@)", error.domain, [NSNumber numberWithInteger:error.code]);
-    NSAlert *alert = [NSAlert alertWithError:error];
-    [alert runModal];
-    [NSApp terminate:self];
-  }
-  
+    // Install helper tools
+    if (//[DKInstaller isInstalled] == NO &&
+        [DKInstaller install:&error] == NO)
+    {
+        assert(error != nil);
+        
+        NSLog(@"Couldn't install MachInjectSample (domain: %@ code: %@)", error.domain, [NSNumber numberWithInteger:error.code]);
+        NSAlert *alert = [NSAlert alertWithError:error];
+        [alert runModal];
+        [NSApp terminate:self];
+    }
     
-    int64_t delayInSeconds = 12.0;
-    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
-    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-        // Inject Finder process
-        NSError *error = nil;
-        if ([DKInjectorProxy inject:&error] == FALSE) {
-            assert(error != nil);
-            
-            NSLog(@"Couldn't inject Finder (domain: %@ code: %@)", error.domain, [NSNumber numberWithInteger:error.code]);
-            NSAlert *alert = [NSAlert alertWithError:error];
-            [alert runModal];
-            [NSApp terminate:self];
-        }
-
-    });
-   
+    if ([DKInjectorProxy inject:&error] == FALSE) {
+        assert(error != nil);
+        
+        NSLog(@"Couldn't inject Finder (domain: %@ code: %@)", error.domain, [NSNumber numberWithInteger:error.code]);
+        NSAlert *alert = [NSAlert alertWithError:error];
+        [alert runModal];
+        [NSApp terminate:self];
+    }
+    
+    //During debug I needed to pause the XPC connections so they wouldn't collide
+    /*
+     int64_t delayInSeconds = 12.0;
+     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+     dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+     // Inject Finder process
+     NSError *error = nil;
+     
+     });*/
+    
 }
 
 @end
